9b34068d1134e869edc64564d10fb966b6c9aadf
unknown <ziane.aymn@gmail.com>
2026-01-03 11:20:19 +0100

    Fix logo HTML structure in login page

--- src/app/pages/auth/login.component.ts
+++ src/app/pages/auth/login.component.ts
@@ -14,7 +14,9 @@ import { AuthService } from '../../services/auth.service';
         <div class="auth-card">
           <div class="auth-header">
             <div class="logo">
-              <img src="assets/logo.png" alt="FacturePro" class="logo-img">
+              <div class="logo-box">
+                <img src="assets/logo.png" alt="FacturePro" class="logo-img">
+              </div>
               <span class="logo-text">FacturePro</span>
             </div>
             <h1>Connexion</h1>
